7c3459fe93a099c7637729da5c2853fa0ddd3f3f
Thang Pham <pmthangk09@gmail.com>
2022-08-02 17:45:40 -0500

    Export results to CSV file

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# openreview_stats
+# Openreview Stats
+
+This is a fun project that helps researchers who submitted papers to any conferences from Openreview to easily retrieve and compare their review scores with others.
+You may have better strategies for your rebuttal period if you know the current rank among the submissions.
+We can also gain more insights from the rebuttal and see if it actually helps increase your change of acceptance by comparing review scores before and after the rebuttal (Note: Data will be updated after the rebuttal).
+
+**TLDR**: *We collected [review scores](https://docs.google.com/spreadsheets/d/1OhSK5jimCyPNtpOmO05vB547X6LhVmsklKgi3p_4WUc/edit?usp=sharing) of the Dataset and Benchmark track for NeurIPS which has just been organized since 2021.*
+
+If you are interested or have some ideas to improve this project, please feel free to reach out to me (Thang Pham) at [thangpham@auburn.edu](thangpham@auburn.edu).
+
+### Prerequisites
+
+* Anaconda 4.10 or higher
+* Python 3.9 or higher
+* pip version 21 or higher
+
+### Installation
+
+1. Clone this repository
+
+   ```sh
+   git clone git@github.com:ThangPM/openreview_stats.git && cd openreview_stats
+   ```
+
+2. Create and activate a Conda environment
+
+   ```sh
+   conda create -n openreview_stats python=3.9
+   conda activate openreview_stats
+   ```
+
+3. Install required libraries
+
+   ```sh
+   pip install -r requirements.txt
+   ```
+4. Run the code to retrieve review scores for accepted papers (NeurIPS-2021) and under-reviewed papers (NeurIPS-2022).
+
+    ```python
+    python3 openreview_stats.py
+    ```
+
+### Collecting Results
+
+Results are stored under `results` folder.
+* results/neurips2021.csv
+* results/neurips2022.csv
--- openreview_stats.py
+++ openreview_stats.py
@@ -1,29 +1,34 @@
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
-import statistics
 from tqdm import tqdm
+
+import pandas as pd
+import statistics
 
 
 if __name__ == '__main__':
+
     openreview_url = "https://openreview.net"
     confs = {"neurips2021": 5, "neurips2022": 8}
+    column_names = ["Paper_title", "URL", "Review_scores", "Average"]
 
     session = HTMLSession()
 
     for conf, no_pages in confs.items():
         print("Extracting review scores for conference {}...".format(conf))
 
+        results = []
         for i in range(0, no_pages):
             print("Extracting at page {}...".format(i+1))
-            results = {}
-            html = ""
 
+            html = ""
             with open("data/{}/page{}.txt".format(conf, i+1), "r") as input_file:
                 for line in input_file:
                     html += line
 
             soup = BeautifulSoup(html, features='lxml')
             submissions = [(a.text.replace("\n", "").strip(), a.attrs['href']) for a in soup.find_all('a', href=True) if a.attrs['href'].startswith("/forum?id=")]
+            page_results = []
 
             for submission in tqdm(submissions):
                 sub_url = openreview_url + submission[1]
@@ -35,10 +40,15 @@ if __name__ == '__main__':
                 review_scores = [int(score.split(":")[0]) for idx, score in enumerate(all_scores) if idx % 2 == 0]
                 confidence_scores = [int(score.split(":")[0]) for idx, score in enumerate(all_scores) if idx % 2 == 1]
 
-                results[openreview_url + submission[1]] = "Title:\t{}\tURL:\t{}\tReview_scores\t{}\tAverage:\t{}".format(submission[0], openreview_url + submission[1], review_scores, statistics.mean(review_scores) if len(review_scores) > 0 else 0)
+                results.append([submission[0], openreview_url + submission[1], review_scores, statistics.mean(review_scores) if len(review_scores) > 0 else 0])
+                page_results.append(results[-1])
 
-            for key, value in results.items():
-                print(value)
+            [print("Title:\t{0}\tURL:\t{1}\tReview_scores\t{2}\tAverage:\t{3}".format(*result)) for result in page_results]
+
+        df = pd.DataFrame(results, columns=column_names)
+        df.sort_values(by=["Average"])
+        df["Rank"] = list(range(1, len(df) + 1))
+        df.to_csv("results/{}.csv".format(conf))
 
     print("Finish!")
 
